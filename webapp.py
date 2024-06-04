ea87f2ad8cb9ea42c0614e6928b303763cd0c4d4
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-06-04 18:16:36 +0300

    Fixed the key error issue

--- webapp.py
+++ webapp.py
@@ -16,11 +16,12 @@ st.subheader("This is my todo app.")
 st.write("This app is to help you enhance your productivity")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=index)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        if todo in st.session_state:
+            del st.session_state[todo]
         st.rerun()
 
 st.text_input(label="Enter a todo:", placeholder="Add new todo...",
